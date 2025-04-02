0318ce5cf53b0c4117da0b9c4c12ff095ea681e9
alok <alok9601377611@gmail.com>
2025-04-03 00:00:28 +0530

    new repo project about jQuery

---
+++ app.js
@@ -0,0 +1,25 @@
+// $("#btn").click(function(){
+    //     // $(".navy").hide();
+    
+    //     // for show()
+    //     $(".forest-green").fadeOut();
+    // })
+    // $("#btn2").click(function(){
+    //     $(".navy").fadeIn();
+    //     $(".forest-green").fadeIn();
+    // })
+
+    // Slides Effects...
+    // $("#btn").click(function(){
+    //     // $(".navy").slideUp();
+    //     $(".forest-green").slideToggle();
+    // })
+
+    // animate the object
+    $("#btn").click(function(){
+        $(".navy").animate({
+            width: "20rem",
+            height: "20rem",
+            left: "-250px"
+        })
+    })
